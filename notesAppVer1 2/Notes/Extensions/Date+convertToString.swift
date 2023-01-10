a935ad87ccb109ed9ec266ad76496dd16b929a16
Roza Sharkhmullina <rossieshark@gmail.com>
2023-01-10 10:22:28 +0300

    downloading all files

---
+++ notesAppVer1 2/Notes/Extensions/Date+convertToString.swift
@@ -0,0 +1,19 @@
+//
+// Date+convertToString.swift
+//  NotesApp
+//  Created by Roza Sharkhmullina on 27/12/22.
+//
+
+import Foundation
+
+extension Date {
+    func convertToString() -> String {
+        return DateFormatter.localizedString(from: self, dateStyle: DateFormatter.Style.medium, timeStyle: DateFormatter.Style.medium)
+    }
+}
+
+extension NSDate {
+    func convertToString() -> String {
+        return DateFormatter.localizedString(from: (self as Date), dateStyle: DateFormatter.Style.medium, timeStyle: DateFormatter.Style.medium)
+    }
+}
---
+++ notesAppVer1 2/Notes/Views/ListNotesTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ListNotesTableViewCell.swift
+//  NotesApp
+//  Created by Roza Sharkhmullina on 27/12/22.
+//
+
+import UIKit
+
+class ListNotesTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var noteTitleLabel: UILabel!
+    @IBOutlet weak var noteModificationTimeLabel: UILabel!
+    
+}
+
